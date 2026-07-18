20185c28ebd02baf2777bddd4ab9c7f60b858029
alsabith-gif <muhammedalsabith111@gmail.com>
2026-07-18 15:30:37 +0530

    Update content.js

--- WELIZA_PUBLIC/content.js
+++ WELIZA_PUBLIC/content.js
@@ -100,7 +100,7 @@ const SITE_CONTENT = {
       category: "prayer",
       desc: "Pure cotton. Pure white.Soft against the skin, light through long hours of wear.Made for comfort that stays with you, prayer after prayer.",
       spec: "100% Organic Cotton| All Size",
-      image: "assets/catalog/Crepe prayer dress.png"
+      image: "assets/catalog/white cotton prayer dresss.png"
     },
     {
       id: 2,
